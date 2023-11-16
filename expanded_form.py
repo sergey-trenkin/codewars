dd29785c9b560c78d65b1ce4c92cad9477205ec2
Sergey <140949059+sergey-trenkin@users.noreply.github.com>
2023-11-16 11:59:20 +0300

    Create expanded_form.py

---
+++ expanded_form.py
@@ -0,0 +1,15 @@
+# Task
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+# expanded_form(12) # Should return '10 + 2'
+# expanded_form(42) # Should return '40 + 2'
+# expanded_form(70304) # Should return '70000 + 300 + 4'
+
+def expanded_form(num):
+    result = []
+
+    for index, digit in enumerate(str(num)[::-1]):
+        if int(digit) != 0:
+            result.append(digit + ('0' * index))
+
+    return ' + '.join(result[::-1])
